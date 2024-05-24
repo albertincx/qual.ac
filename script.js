b5ce8a8bff7af8a3977234793a76f171ec9ecb21
albertincx <albertincx@gmail.com>
2024-05-24 12:15:06 +0700

    init

--- script.js
+++ script.js
@@ -16,6 +16,26 @@ subscribeBtn.addEventListener('click', function(event) {
         // Perform further actions, such as sending the subscription request
         messageDiv.textContent = 'Thank you for subscribing!';
         messageDiv.style.color = 'green';
-        emailInput.value = '';
+        window.__arsfChatIdg='1002238842457_S';
+        window.__arsfChatUrl = 'api.cafechat.app';
+        var newScript = document.createElement('script');
+        newScript.type = 'text/javascript';
+        newScript.src = '//cafechat.app/start.js';
+        newScript.onload = () => {
+            setTimeout(function() {
+                document.querySelector('.messanger-button').click();
+                setTimeout(function() {
+                    const message = { service: '', g: window.__arsfChatIdg, message: `subscribe ${emailInput.value}` }
+                    if (window.__arsfChat) {
+                        window.__arsfChat.send(JSON.stringify(message));
+                        emailInput.value = '';
+                    }
+                    if (window.__arsfChatDestroy) {
+                        window.__arsfChatDestroy();
+                    }
+                }, 1500);
+            }, 1000)
+        };
+        document.getElementsByTagName("head")[0].appendChild(newScript);
     }
 });

0134c1bd0192843a2e329e1f2dfbebc25e099498
albertincx <albertincx@gmail.com>
2024-05-25 12:04:10 +0700

    init

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const subscribeBtn = document.getElementById('subscribe-btn');
 const emailInput = document.getElementById('email');
 const messageDiv = document.getElementById('message');
+let chatLoaded
 
-subscribeBtn.addEventListener('click', function(event) {
+const connectCb = () => {
+    if (window.__arsfChat) {
+        window.__arsfChat.sendMessage(`subscribe ${emailInput.value}`);
+        emailInput.value = '';
+    }
+    if (window.__arsfChatDestroy) {
+        window.__arsfChatDestroy();
+    }
+};
+
+const OnLoad = () => {
+    setTimeout(() => {
+        if (!window.__arsfChatConnect) {
+            return;
+        }
+        chatLoaded = true;
+        window.__arsfChatConnect(false, {
+            cb: connectCb
+        });
+    }, 1000)
+};
+
+subscribeBtn.addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the form from submitting
 
     const email = emailInput.value.trim();
@@ -16,26 +39,16 @@ subscribeBtn.addEventListener('click', function(event) {
         // Perform further actions, such as sending the subscription request
         messageDiv.textContent = 'Thank you for subscribing!';
         messageDiv.style.color = 'green';
-        window.__arsfChatIdg='1002238842457_S';
-        window.__arsfChatUrl = 'api.cafechat.app';
-        var newScript = document.createElement('script');
-        newScript.type = 'text/javascript';
-        newScript.src = '//cafechat.app/start.js';
-        newScript.onload = () => {
-            setTimeout(function() {
-                document.querySelector('.messanger-button').click();
-                setTimeout(function() {
-                    const message = { service: '', g: window.__arsfChatIdg, message: `subscribe ${emailInput.value}` }
-                    if (window.__arsfChat) {
-                        window.__arsfChat.send(JSON.stringify(message));
-                        emailInput.value = '';
-                    }
-                    if (window.__arsfChatDestroy) {
-                        window.__arsfChatDestroy();
-                    }
-                }, 1500);
-            }, 1000)
-        };
-        document.getElementsByTagName("head")[0].appendChild(newScript);
+        if (chatLoaded) {
+            connectCb();
+        } else {
+            window.__arsfChatIdg = '1002238842457_S';
+            window.__arsfChatUrl = 'api.cafechat.app';
+            var newScript = document.createElement('script');
+            newScript.type = 'text/javascript';
+            newScript.src = '//cafechat.app/start.js';
+            newScript.onload = OnLoad();
+            document.getElementsByTagName("head")[0].appendChild(newScript);
+        }
     }
 });
